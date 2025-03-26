82af60364db2e647b97c054dcbe6818e6acc699f
yarml <youssefharmal@gmail.com>
2025-03-26 14:11:01 +0000

    [x64] implement (VirtAddr|PhysAddr)::(to_ref|to_mut)

--- x64/src/mem/addr/phys.rs
+++ x64/src/mem/addr/phys.rs
@@ -9,6 +9,9 @@ use super::virt::VirtAddr;
 
 const PHYS_MASK: usize = 0x00FF_FFFF_FFFF_FFFF;
 
+// Bootloader keeps this at 0, since it always has identity mapping until the
+// last moment when it doesn't need direct access anymore. Kernel sets this to
+// the offset it finds in BootInfo as soon as it gets control.
 static PHYSICAL_MEMORY_OFFSET: AtomicUsize = AtomicUsize::new(0);
 
 define_addr!(PhysAddr, make_canonical);
@@ -29,6 +32,30 @@ impl PhysAddr {
     #[inline]
     pub fn to_virt(&self) -> VirtAddr {
         VirtAddr::new_truncate(self.inner + PHYSICAL_MEMORY_OFFSET.load(Ordering::Relaxed))
+    }
+
+    #[inline]
+    /// # Safety
+    /// Must ensure that physical memory offset is set and valid
+    /// and that the memory location contains a valid instance of T
+    /// and that the memory location is not mutably aliased
+    pub unsafe fn to_ref<'a, T>(&self) -> &'a T {
+        unsafe {
+            // SAFETY: ensured by caller
+            &*self.to_virt().as_ptr()
+        }
+    }
+
+    #[inline]
+    /// # Safety
+    /// Must ensure that physical memory offset is set and valid
+    /// and that the memory location contains a valid instance of T
+    /// and that the memory location is not aliased
+    pub unsafe fn to_mut<'a, T>(&self) -> &'a mut T {
+        unsafe {
+            // SAFETY: ensured by caller
+            &mut *self.to_virt().as_mut_ptr()
+        }
     }
 }
 
--- x64/src/mem/addr/virt.rs
+++ x64/src/mem/addr/virt.rs
@@ -9,6 +9,28 @@ impl VirtAddr {
     pub const fn page<S: PageSize>(&self) -> Page<S> {
         Page::containing(*self)
     }
+
+    #[inline]
+    /// # Safety
+    /// Must ensure that the memory location contains a valid instance of T
+    /// and that the memory location is not mutably aliased
+    pub unsafe fn to_ref<'a, T>(&self) -> &'a T {
+        unsafe {
+            // SAFETY: ensured by caller
+            &*self.as_ptr()
+        }
+    }
+
+    #[inline]
+    /// # Safety
+    /// Must ensure that the memory location contains a valid instance of T
+    /// and that the memory location is not aliased
+    pub unsafe fn to_mut<'a, T>(&self) -> &'a mut T {
+        unsafe {
+            // SAFETY: ensured by caller
+            &mut *self.as_mut_ptr()
+        }
+    }
 }
 
 #[inline]
