045d47154a5f08d3aa1376e3ed249e5e48ea3fcd
yarml <youssefharmal@gmail.com>
2025-03-29 23:12:24 +0000

    [boot-protocol] add FeatureSet to BootInfo

--- boot-protocol/src/lib.rs
+++ boot-protocol/src/lib.rs
@@ -2,13 +2,15 @@
 
 pub mod features;
 
+use features::FeatureSet;
 use x64::mem::MemoryRegion;
 
 const MMAP_PG_COUNT: usize = 1;
 pub const MAX_MMAP_SIZE: usize = MMAP_PG_COUNT * (4096 / core::mem::size_of::<MemoryRegion>());
 
-#[repr(C)]
+#[repr(C, align(4096))]
 pub struct BootInfo {
     pub mmap: [MemoryRegion; MAX_MMAP_SIZE],
     pub mmap_len: usize,
+    pub features: FeatureSet,
 }
