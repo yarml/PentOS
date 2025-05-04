995f266edcba39ed937bda4c85555e149d379b7a
yarml <youssefharmal@gmail.com>
2025-05-04 17:46:09 +0100

    [bootloader] implement PIT assisted sleep_us

--- bootloader/src/pic.rs
+++ bootloader/src/pic.rs
@@ -1,11 +1,11 @@
 use x64::io::Port;
 use x64::io::{self};
 
-const PIC_MASTER_CMD: Port<u8> = Port::new(0x20);
-const PIC_MASTER_DATA: Port<u8> = Port::new(0x21);
+const MASTER_CMD: Port<u8> = Port::new(0x20);
+const MASTER_DATA: Port<u8> = Port::new(0x21);
 
-const PIC_SLAVE_CMD: Port<u8> = Port::new(0xA0);
-const PIC_SLAVE_DATA: Port<u8> = Port::new(0xA1);
+const SLAVE_CMD: Port<u8> = Port::new(0xA0);
+const SLAVE_DATA: Port<u8> = Port::new(0xA1);
 
 // Intact code from HeliumOS
 pub fn disable() {
@@ -14,28 +14,28 @@ pub fn disable() {
         // None of these have any side effect on memory
 
         // Config mode
-        PIC_MASTER_CMD.write(0x11);
-        PIC_SLAVE_CMD.write(0x11);
+        MASTER_CMD.write(0x11);
+        SLAVE_CMD.write(0x11);
         io::wait();
 
         // Offset master and slave to 0x20 and 0x28 respectively
-        PIC_MASTER_DATA.write(0x20);
-        PIC_SLAVE_DATA.write(0x28);
+        MASTER_DATA.write(0x20);
+        SLAVE_DATA.write(0x28);
         io::wait();
 
         // Configure master slave relationship
-        PIC_MASTER_DATA.write(4);
-        PIC_SLAVE_DATA.write(2);
+        MASTER_DATA.write(4);
+        SLAVE_DATA.write(2);
         io::wait();
 
         // Use 8086 Mode
-        PIC_MASTER_DATA.write(1);
-        PIC_SLAVE_DATA.write(1);
+        MASTER_DATA.write(1);
+        SLAVE_DATA.write(1);
         io::wait();
 
         // Mask everything
-        PIC_MASTER_DATA.write(0xFF);
-        PIC_SLAVE_DATA.write(0xFF);
+        MASTER_DATA.write(0xFF);
+        SLAVE_DATA.write(0xFF);
 
         io::wait();
     }
---
+++ bootloader/src/pit.rs
@@ -0,0 +1,39 @@
+use core::hint;
+
+use x64::io::Port;
+
+const CH0_DATA: Port<u8> = Port::new(0x40);
+const CMD: Port<u8> = Port::new(0x43);
+
+const BASE_FREQ: usize = 1193182;
+
+/// Sleep with a precision of 5us and accuracy > 99% (not counting hardware accuracy) for AT LEAST t us
+pub fn sleep_us(t: usize) {
+    const TU_PER_5US: usize = 6;
+    let us5_amount = (t / 5) + 1;
+    let delta_units = us5_amount * TU_PER_5US;
+    let expected_delivery_time = 0xFF - delta_units;
+
+    // Start timer
+    unsafe {
+        // # Safety
+        // No side effect
+        CH0_DATA.write(0xFF);
+        CH0_DATA.write(0xFF);
+    }
+    loop {
+        let current_time = unsafe {
+            // # Safet
+            // No side effect
+            CMD.write(0);
+            let lo = CH0_DATA.read();
+            let hi = CH0_DATA.read();
+            lo as u16 | (hi as u16) << 8
+        } as usize;
+
+        if current_time < expected_delivery_time {
+            break;
+        }
+        hint::spin_loop();
+    }
+}
