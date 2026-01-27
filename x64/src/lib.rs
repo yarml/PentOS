12f103f4ffd9cc27eb048b27323b836ef777b470
yarml <youssefharmal@gmail.com>
2026-01-28 00:31:47 +0100

    [x64] remove unnecessary incomplete features

--- x64/src/lib.rs
+++ x64/src/lib.rs
@@ -1,10 +1,7 @@
 #![no_std]
-#![allow(incomplete_features)] // FIXME: I don't want to resort to this
 #![feature(cold_path)]
 #![feature(abi_x86_interrupt)]
 #![feature(const_trait_impl)]
-// We going really hardcore with this one
-#![feature(non_lifetime_binders)]
 
 #[cfg(test)]
 extern crate alloc;
